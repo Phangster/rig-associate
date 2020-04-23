e20827f5a7aaa67dfbe17fbcca61134dbe2a1d35
Daryll Wong <daryll_wong@hotmail.com>
2020-04-24 03:15:32 +0800

    jesus will save this

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-//import { HashRouter as Router, Route } from "react-router-dom";
+//import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Homepage";
 import Raffles from "./components/Raffles";
 import Sompo from "./components/Sompo";
