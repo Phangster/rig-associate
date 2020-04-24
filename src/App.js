10e1f351e14a56cb1c6044b2ebb76d60df6ec8ad
Bryan <mynamebryanphang@gmail.com>
2020-04-24 12:40:11 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 //import { BrowserRouter as Router, Route } from "react-router-dom";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, BrowserRouter, Switch } from "react-router-dom";
 import Home from "./components/Homepage";
 import Raffles from "./components/Raffles";
 import Sompo from "./components/Sompo";
@@ -13,15 +13,21 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-        // <Router basename={process.env.PUBLIC_URL}>
+      <div>
+      <BrowserRouter>
+        {/* // <Router basename={process.env.PUBLIC_URL}> */}
           <div className="App">
+          <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/raffleshealthinsurance" component={Raffles} />
             <Route exact path="/sompoinsurance" component={Sompo} />
             <Route exact path="/nowhealthmedical" component={Now} />
             <Route exact path="/enquiryform" component={EnquiryForm} />
+           </Switch>
           </div>
-        // </Router>
+        {/* // </Router> */}
+      </BrowserRouter>
+      </div>
     );
   }
 }
